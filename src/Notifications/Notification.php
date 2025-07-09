fbab9e18364d927793a938174d710627dedf11c1
Akeeb Ismail <damiz.akeeb@gmail.com>
2025-07-09 18:29:06 +0100

    add notification class

---
+++ src/Notifications/Notification.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace FastFast\Common\Notifications;
+
+use App\Models\Admin;
+use App\Models\Notification as Model_Notification;
+use App\Models\User;
+use App\Notifications\OrderStatusNotification;
+use Carbon\Carbon;
+use Kreait\Firebase\Exception\FirebaseException;
+use Kreait\Firebase\Exception\MessagingException;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Pusher\Pusher;
+use Pusher\PushNotifications\PushNotifications;
+
+class Notification {
+
+    private function getFirebaseInstance()
+    {
+        return (new Factory)->withServiceAccount(storage_path('app/firebase') .'/fastfast-firebase.json')->createMessaging();
+    }
+
+    private function getBeamInstance()
+    {
+        return new PushNotifications([
+            "instanceId" => config('services.pusher-beams.instance_id'),
+            "secretKey" => config('services.pusher-beams.secret_key'),
+        ]);
+    }
+
+    private function getPusherInstance()
+    {
+        return new Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'), [
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+        ]);
+    }
+
+    /**
+     * @throws MessagingException
+     * @throws FirebaseException
+     */
+    private function validateFirebaseToken(Messaging $messaging, $device_tokens)
+    {
+        $result = $messaging->validateRegistrationTokens($device_tokens);
+        $resp = [];
+        if(isset($result['valid']))
+        {
+            $resp = $result['valid'];
+        }
+        return $resp;
+    }
+
+    private function generateFirebaseNotification($title, $body)
+    {
+        return \Kreait\Firebase\Messaging\Notification::create($title, $body);
+    }
+
+    public function sendToMultiDevices($device_tokens, $title, $body, $data)
+    {
+        $messaging = $this->getFirebaseInstance();
+        $device_tokens = $this->validateFirebaseToken($messaging, $device_tokens);
+        $resp = null;
+        if(count($device_tokens) > 0)
+        {
+            $notification = $this->generateFirebaseNotification($title, $body);
+            $message = CloudMessage::new();
+            $message = $message->withNotification($notification);
+            if(count($data) > 0)
+            {
+                $message = $message->withData($data);
+            }
+            $resp = $messaging->sendMulticast($message, $device_tokens)->successes();
+        }
+        return $resp;
+    }
+
+    private function generateBeamToken($id, $is_admin=false)
+    {
+        if($is_admin == true)
+        {
+            $id = "admin_id_" . $id;
+        }
+        else
+        {
+            $id = "user_id_" . $id;
+        }
+        $beam_instance = $this->getBeamInstance();
+        $beam_token = $beam_instance->generateToken($id);
+        return [
+            'token' => $beam_token['token'],
+            'user_id' => $id,
+        ];
+    }
+
+    private function manageAdminBeamToken(Admin $admin)
+    {
+        if(empty($admin->beam_user_id))
+        {
+            $attempt = $this->generateBeamToken($admin->id, true);
+            $token = "";
+            if(is_array($attempt))
+            {
+                if(isset($attempt['token']))
+                {
+                    Admin::find($admin->id)->update(['beam_token' => $attempt['token'], 'beam_user_id' => $attempt['user_id'], 'beam_expire_at' => now()->addHours(24)]);
+                    $token = $attempt['user_id'];
+                }
+            }
+            return $token;
+        }
+        else
+        {
+            $exp = new Carbon($admin->beam_expire_at);
+            $token = "";
+            if($exp->isPast())
+            {
+                $attempt = $this->generateBeamToken($admin->id, true);
+                if(is_array($attempt))
+                {
+                    if(isset($attempt['token']))
+                    {
+                        Admin::find($admin->id)->update(['beam_token' => $attempt['token'], 'beam_user_id' => $attempt['user_id'], 'beam_expire_at' => now()->addHours(24)]);
+                        $token = $attempt['user_id'];
+                    }
+                }
+            }
+            else
+            {
+                $token = $admin->beam_user_id;
+            }
+            return $token;
+        }
+    }
+    public function manageUserBeamToken(User $user)
+    {
+        if(empty($user->beam_user_id))
+        {
+            $attempt = $this->generateBeamToken($user->id, false);
+            $token = "";
+            if(is_array($attempt))
+            {
+                if(isset($attempt['token']))
+                {
+                    User::find($user->id)->update(['beam_token' => $attempt['token'], 'beam_user_id' => $attempt['user_id'], 'beam_expire_at' => now()->addHours(24)]);
+                    $token = $attempt['user_id'];
+                }
+            }
+        }
+        else
+        {
+            $exp = new Carbon($user->beam_expire_at);
+            $token = "";
+            if($exp->isPast())
+            {
+                $attempt = $this->generateBeamToken($user->id, false);
+                if(is_array($attempt))
+                {
+                    if(isset($attempt['token']))
+                    {
+                        User::find($user->id)->update(['beam_token' => $attempt['token'], 'beam_user_id' => $attempt['user_id'], 'beam_expire_at' => now()->addHours(24)]);
+                        $token = $attempt['user_id'];
+                    }
+                }
+            }
+            else
+            {
+                $token = $user->beam_user_id;
+            }
+        }
+        return $token;
+    }
+
+    public function getAdminsBeamTokens($role=0)
+    {
+        $admins = Admin::select('id', 'beam_token', 'beam_expire_at');
+        if($role > 0)
+        {
+            $admins = $admins->where('role', '=', $role);
+        }
+        $admins = $admins->get();
+        $ret_arr = [];
+        if($admins->count() > 0)
+        {
+            foreach($admins as $admin)
+            {
+                $ret_arr[] = $this->manageAdminBeamToken($admin);
+            }
+        }
+        foreach($ret_arr as $key => $value)
+        {
+            if(empty($value))
+            {
+                unset($ret_arr[$key]);
+            }
+        }
+        return $ret_arr;
+    }
+
+    public function sendBeamMessage($tokens, $title, $body, $data = [])
+    {
+        $beam_instance = $this->getBeamInstance();
+        return $beam_instance->publishToUsers($tokens, [
+            "apns" => [
+                "aps" => [
+                    "alert" => [
+                        "title" => $title,
+                        "body" => $body,
+                    ],
+                ],
+                'custom' => $data,
+            ],
+            "fcm" => [
+                "notification" => [
+                    "title" => $title,
+                    "body" => $body,
+                ],
+                'data' => $data,
+            ],
+            "web" => [
+                "notification" => [
+                    "title" => $title,
+                    "body" => $body,
+                ],
+                'data' => $data,
+            ],
+        ]);
+    }
+
+    public function sendPusherMessage($event, $data=[])
+    {
+        $pusher_instance = $this->getPusherInstance();
+        return $pusher_instance->trigger("FastFast", $event, $data);
+    }
+
+    public function sendFirebaseMessage($device_token, $title, $body, $data, $userId = null, $status = 'created')
+    {
+        $user = $userId ? User::find($userId) : null;
+        if ($user && $user->device_type == 'ios') {
+            return $user->notify(new OrderStatusNotification($status, $data));
+            //$type = $user->user_type == 1 ? 'customer' : ($user->user_type == 3 ? 'rider' : 'seller');
+            //return $this->sendUserAPNS($data, $user, $type);
+        }
+        $messaging = $this->getFirebaseInstance();
+        $device_tokens = $this->validateFirebaseToken($messaging, $device_token);
+        $resp = null;
+        if(count($device_tokens) > 0)
+        {
+            $device_token = $device_tokens[0];
+            $notification = $this->generateFirebaseNotification($title, $body);
+            $message = CloudMessage::withTarget('token', $device_token)->withNotification($notification);
+            if(count($data) > 0)
+            {
+                $message = $message->withData($data);
+            }
+            $resp = $messaging->send($message);
+        }
+        return [
+            'status' => true,
+            'message' => 'Success',
+            'data' => $resp,
+        ];
+    }
+
+    public function createNotification($data)
+    {
+        return Model_Notification::create($data);
+    }
+}
